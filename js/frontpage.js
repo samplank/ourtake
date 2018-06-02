cc5baa6f0c85a252bf856409514cb5b5cace6313
Sam Plank <samuelplank@college.harvard.edu>
2018-06-02 13:58:40 -0400

    add credits button

--- js/frontpage.js
+++ js/frontpage.js
@@ -168,7 +168,6 @@ function writeUserData(userId, name, email) {
 
 function increaseCredits() {
 	console.log(user);
-	var databaseRef = firebase.database().ref('users/' + user.uid + '/credits');
 	var databaseRef = firebase.database().ref('users').child(user.uid).child('credits');
 	console.log(databaseRef);
 

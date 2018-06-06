e6224fc311aabb03d9a71b6a7541a88b21a83488
Sam Plank <samuelplank@college.harvard.edu>
2018-06-06 15:02:17 -0600

    add credits and votes to clicks

--- js/article.js
+++ js/article.js
@@ -183,7 +183,7 @@ function submitText(i,articleID) {
 
             var now = new Date().getTime();
 
-            var contributionID = writeNewContribution(textInput,0,0,false,user.uid,now,articleID);
+            var contributionID = writeNewContribution(textInput,0,0,false,user.displayName,now,articleID);
 
             loadText(articleID);
             var updates = {};

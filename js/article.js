b1e698dff32e96c028909338705e3e7aa72b7583
Sam Plank <samuelplank@college.harvard.edu>
2018-07-30 22:15:39 -0400

    last 3 paragraphs

--- js/article.js
+++ js/article.js
@@ -72,7 +72,7 @@ function loadText(articleID) {
     waitForParaCount();
 
     function waitForParaCount() {
-      if (typeof paragraph_count !== "undefined"){
+      if (typeof paragraph_count !== "number"){
       //variable exists, do what you want
 
         // firebase.database().ref('posts/'  + String(articleID) + '/paragraph_count').once('value').then(function(snapshot) {

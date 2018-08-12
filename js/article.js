321011cbebdfb4bf16409efd0ff8804046502a0f
Sam Plank <samuelplank@college.harvard.edu>
2018-08-13 00:19:08 +0800

    styling

--- js/article.js
+++ js/article.js
@@ -32,7 +32,7 @@ function updateUser(userUpdate) {
       var signOutButton = document.createElement("button");
       signOutButton.id = "signOut";
       signOutButton.className = "topButton";
-      signOutButton.innerHTML = credits + ': ' + user.displayName;
+      signOutButton.innerHTML = "VoteClout: " + "<span style='color:#b36b00;'>" + credits + "</span>";
       signOutButton.addEventListener('click', function(event) {
         firebase.auth().signOut();
       });

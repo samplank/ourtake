a0e3a19fe6c570399331070c69e2202a51500b92
Sam Plank <samuelplank@college.harvard.edu>
2018-09-02 19:26:00 +0800

    add first para

--- js/frontpage.js
+++ js/frontpage.js
@@ -64,12 +64,11 @@ function loadArticles() {
         postRef.child('contributions').orderByChild('paragraph_number').equalTo(1).on("value", function(snapshot) {
             val = snapshot.val();
             body = val.body;
+
+            aref.innerHTML = "<span style='font-weight:bold;'>" + contribution.title + "</span>" + '\n' + body;
+
+            articleArray.push(link);
         });
-
-
-        aref.innerHTML = "<span style='font-weight:bold;'>" + contribution.title + "</span>" + '\n' + body;
-
-        articleArray.push(link);
 
     });
 

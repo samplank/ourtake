e051a12656560a4fde2d8ebe91b0d35e20141f46
Sam Plank <samuelplank@college.harvard.edu>
2018-09-04 19:24:17 +0800

    change addcredits

--- js/frontpage.js
+++ js/frontpage.js
@@ -12,11 +12,11 @@ function updateUser(userUpdate) {
         authDiv.removeChild(authDiv.firstChild);
       }
 
-      var creditButton = document.createElement("button");
-      creditButton.className = "topButton";
+      var creditButton = document.createElement("p");
+      // creditButton.className = "topButton";
       var creditRef = firebase.database().ref('users/' + user.uid + '/credits');
       creditRef.on('value', function(snapshot) {
-        creditButton.innerHTML = "<span style='color:#fc643f;'>SlicedCredit: </span>" + snapshot.val();
+        creditButton.innerHTML = "<span style='color:#fc643f;'>SlicedCredit: </span>" + snapshot.val(); + "+"
       });
       authDiv.appendChild(creditButton);
 

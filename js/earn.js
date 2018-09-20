cf91a5defb8d4ff57df5e4db6a85f72f77e77566
Sam Plank <samuelplank@college.harvard.edu>
2018-09-20 01:02:11 +0100

    earn

--- js/earn.js
+++ js/earn.js
@@ -202,11 +202,10 @@ function shuffle(array) {
   return array;
 }
 
-// function getRadioValue(formname) {
-//   var form = document.getElementById(formname);
-//   if (document.getElementById('r1').checked) {
-//   rate_value = document.getElementById('r1').value;
-// }
+function getRadioValue(formname) {
+  var checkedButton = document.querySelector('input[name=' + formname + '][checked]')
+  console.log(checkedButton);
+}
 
 }
 

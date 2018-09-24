8bf5ef7d111e225bed92338562cd9fb846f40a7b
Sam Plank <samuelplank@college.harvard.edu>
2018-09-24 15:31:06 +0100

    mobile

--- js/frontpage.js
+++ js/frontpage.js
@@ -362,6 +362,7 @@ function checkMobile() {
         ourtakeSheet.insertRule("#logo { display: block; margin-left: auto; margin-right: auto; width: 25%; }", 0);
         ourtakeSheet.insertRule("#topcontainer { width: 100%; padding: 6px 12px; display: flex; }", 0);
         ourtakeSheet.insertRule("#topleft { width: 100%; padding: 6px 12px; display: flex; }", 0);
+        ourtakeSheet.insertRule("#topright { width: 100%; padding: 6px 12px; display: flex; }", 0);
 
     }
 }

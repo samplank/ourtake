81ed969dda7975b9238a2faee1360a11aa4f56a7
Sam Plank <samuelplank@college.harvard.edu>
2018-09-28 13:19:51 -0400

    front page styling

--- js/frontpage.js
+++ js/frontpage.js
@@ -441,7 +441,7 @@ function checkMobile() {
         ourtakeSheet.insertRule(".arrow { display: block; width: 5%; height: 5%; margin: 4% 0%; }", 0);
         ourtakeSheet.insertRule('.topButtonRight { font-family: "Trebuchet MS", sans-serif; font-size: 18px; display: inline-block; background: white; color: black; border-radius: 5px; box-shadow: 1px 1px 1px grey; white-space: nowrap; margin: 5px; height: 50px; vertical-align: top; float: right }', 0);
         ourtakeSheet.insertRule('.topButtonLeft { font-family: "Trebuchet MS", sans-serif; font-size: 18px; display: inline-block; background: white; color: black; border-radius: 5px; box-shadow: 1px 1px 1px grey; white-space: nowrap; margin: 5px; height: 50px; vertical-align: top; float: right }', 0);
-        ourtakeSheet.insertRule('.addToArticle { font-family: "Trebuchet MS", sans-serif; font-size: 22px; display: inline-block; background: #fff4db; color: black; border-radius: 3px; white-space: nowrap; margin: 5px; height: 50px; width: 45%; vertical-align: top; white-space: normal;}', 0);
+        ourtakeSheet.insertRule('.addToArticle { font-family: "Trebuchet MS", sans-serif; font-size: 22px; display: inline-block; background: #fff4db; color: black; border-radius: 3px; white-space: nowrap; margin: 5px; height: 50px; width: 45%; vertical-align: top; white-space: normal; cursor: pointer;}', 0);
         ourtakeSheet.insertRule('.frontHolder { padding: 24px; margin: 10% 0% 0% 0%; }', 0);
     } else if (is_mobile) {
         ourtakeSheet.insertRule("#readcontainer { padding: 0% 0%; }", 0)

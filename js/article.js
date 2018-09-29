fcc041b1ec9b63cdcd0a2223dcdbd062b5bec967
Sam Plank <samuelplank@college.harvard.edu>
2018-09-28 21:28:28 -0400

    contrib styling

--- js/article.js
+++ js/article.js
@@ -773,6 +773,6 @@ function checkMobile() {
         ourtakeSheet.insertRule('.topButtonRight { font-family: "Trebuchet MS", sans-serif; font-size: 24px; display: inline-block; background: white; color: black; width: 31%; border-radius: 5px; box-shadow: 1px 1px 1px grey; white-space: nowrap; margin: 5px; height: 100px; vertical-align: top; float: right }', 0);
         ourtakeSheet.insertRule('.topButtonLeft { font-family: "Trebuchet MS", sans-serif; font-size: 24px; display: inline-block; background: white; color: black; width: 26.75%; border-radius: 5px; box-shadow: 1px 1px 1px grey; white-space: nowrap; margin: 5px; height: 100px; vertical-align: top; float: right }', 0);
         ourtakeSheet.insertRule('.conditional { float: left; padding: 3%; width: 100%; margin: 10px 0px; font-size: 24px; line-height: 48px;}');
-        ourtakeSheet.insertRule('#rightjustify { float: right; padding: 3%; width: 90%; margin: 10px 0px; font-size: 36px; line-height: 100px;}');
+        ourtakeSheet.insertRule('#rightjustify { padding: 3%; width: 90%; margin: 10px 0px; font-size: 36px; line-height: 100px;}');
     }
 }

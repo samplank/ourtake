b3ad1dadd86588081547beede11c8ee4ad087c99
Sam Plank <samuelplank@college.harvard.edu>
2018-09-29 09:56:37 -0400

    contrib styling

--- js/article.js
+++ js/article.js
@@ -776,7 +776,7 @@ function checkMobile() {
         ourtakeSheet.insertRule('.topButtonRight { font-family: "Trebuchet MS", sans-serif; font-size: 24px; display: inline-block; background: white; color: black; width: 31%; border-radius: 5px; box-shadow: 1px 1px 1px grey; white-space: nowrap; margin: 5px; height: 100px; vertical-align: top; float: right }', 0);
         ourtakeSheet.insertRule('.topButtonLeft { font-family: "Trebuchet MS", sans-serif; font-size: 24px; display: inline-block; background: white; color: black; width: 26.75%; border-radius: 5px; box-shadow: 1px 1px 1px grey; white-space: nowrap; margin: 5px; height: 100px; vertical-align: top; float: right }', 0);
         ourtakeSheet.insertRule('.conditional { float: left; padding: 3%; width: 100%; margin: 10px 0px; font-size: 24px; line-height: 34px;}');
-        ourtakeSheet.insertRule('#rightjustify { float: right; width: 100%; padding: 3%; margin: 10px 0px; font-size: 34px; line-height: 48px; color: #484848;}');
+        ourtakeSheet.insertRule('#rightjustify { float: right; width: 95%; padding: 3%; margin: 10px 0px; font-size: 34px; line-height: 48px; color: #484848;}');
         ourtakeSheet.insertRule('.voteButton { background-color: #fff4db; font-family: Georgia, serif; font-size: 30px; width: 40%; height: 50%; margin: 0% 3% 0% 0%;}');
     }
 }

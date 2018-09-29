b9b74f6b9149806960bc00f98c74870212f3d97a
Sam Plank <samuelplank@college.harvard.edu>
2018-09-29 09:45:35 -0400

    contrib styling

--- js/article.js
+++ js/article.js
@@ -777,6 +777,6 @@ function checkMobile() {
         ourtakeSheet.insertRule('.topButtonLeft { font-family: "Trebuchet MS", sans-serif; font-size: 24px; display: inline-block; background: white; color: black; width: 26.75%; border-radius: 5px; box-shadow: 1px 1px 1px grey; white-space: nowrap; margin: 5px; height: 100px; vertical-align: top; float: right }', 0);
         ourtakeSheet.insertRule('.conditional { float: left; padding: 3%; width: 100%; margin: 10px 0px; font-size: 24px; line-height: 34px;}');
         ourtakeSheet.insertRule('#rightjustify { float: right; width: 90%; margin: 10px 0px; font-size: 34px; line-height: 48px;}');
-        ourtakeSheet.insertRule('.voteButton { background-color: #fff4db; font-family: Georgia, serif; font-size: 30px; width: 40%; height: 50%; margin: 0% 10%;}');
+        ourtakeSheet.insertRule('.voteButton { background-color: #fff4db; font-family: Georgia, serif; font-size: 30px; width: 40%; height: 50%; margin: 0% 3%;}');
     }
 }

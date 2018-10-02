7217bda0b81a0342856f5bb3d381e4d27c5739fe
Sam Plank <samuelplank@college.harvard.edu>
2018-10-02 10:40:01 -0400

    author page

--- js/author.js
+++ js/author.js
@@ -157,7 +157,7 @@ function loadAuthor(author) {
 
     if (snapshot.key == user.uid) {
       var processDiv = document.getElementById("processDiv");
-      processDiv.innerHTML = "The Process";
+      processDiv.innerHTML = '<span style="font-weight:bold; font-size: 28px; line-height: 40px; font-family: "Lora";">The Process</span>';
     }
 
     authorName = val.username;

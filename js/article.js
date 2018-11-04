ac0e0cd38b82fd1fe79fe5d224e002b4ef556bd1
Sam Plank <samuelplank@college.harvard.edu>
2018-11-03 19:14:40 -0700

    earn vote

--- js/article.js
+++ js/article.js
@@ -618,7 +618,7 @@ function addEarn(direction, contributionID, articleID) {
 
             function waitForMaxScroll(){
               if (content.style.maxHeight !== null) {
-                window.scroll({left: 0, top: findPos(document.getElementById("earn" + String(contributionID))),behavior: 'smooth'});
+                window.scroll({left: 0, top: findPos(document.getElementById("leftjustify" + String(contributionID))),behavior: 'smooth'});
               }
               else {
                 console.log("wait");
